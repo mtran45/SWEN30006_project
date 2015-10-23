5fcdda809560a5a6c8d5c645c6793b2ee2a23ae2
Michael Tran <mtran45@gmail.com>
2015-10-23 16:41:48 +1100

    Change calculate_weight into instance method

--- app/models/article.rb
+++ app/models/article.rb
@@ -15,7 +15,7 @@ class Article < ActiveRecord::Base
 	  	keywords = search.split
 	  	results = self.all.to_a
 	  	results.each do |article|
-	  		article.weight = calculate_weight(article, keywords)
+	  		article.weight = article.calculate_weight(keywords)
 	  	end
 	  	results.delete_if {|article| article.weight == 0}
 	  	results.sort_by {|article| [article.weight, article.date_of_public] }.reverse
@@ -24,17 +24,17 @@ class Article < ActiveRecord::Base
 	  end
   end
 
-  # Returns the search weighting for an Article, given an array of keywords
-  def self.calculate_weight(article, keywords)
+  # Returns the search weighting for the article, given an array of keywords
+  def calculate_weight(keywords)
   	weight = 0
   	# Downcase keywords for case insensitive search
   	keywords.map! {|word| word.downcase}
   	# Add weightings for each keyword
   	keywords.each do |keyword|
-  		weight += 4 if article.tag_list.any? {|tag| tag.include?(keyword)}
-  		weight += 3 if article.title.downcase.include?(keyword) 
-  		weight += 2 if article.summary.downcase.include?(keyword) 
- 			weight += 1 if article.source.source_name.downcase.include?(keyword) 
+  		weight += 4 if tag_list.any? {|tag| tag.include?(keyword)}
+  		weight += 3 if title.downcase.include?(keyword) 
+  		weight += 2 if summary.downcase.include?(keyword) 
+ 			weight += 1 if source.source_name.downcase.include?(keyword) 
   	end
   	weight
   end
